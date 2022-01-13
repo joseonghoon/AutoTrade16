327de7843b3fbe034cdf713fc4a9f2adcf726d5b
SeongHoon Jo <tjd0544@naver.com>
2022-01-13 10:20:09 +0900

    Update XRPAutoTrade.py

--- XRPAutoTrade.py
+++ XRPAutoTrade.py
@@ -5,7 +5,7 @@ import requests
 
 access = "bsYWp701yJmtgcHxwvVxgAdrlxJYyE8UU1uvMpQa"
 secret = "jZBIU6tTAkrr6aXPZRUSwdPkEGSLiBqdKaPxxORm"
-myToken = "xoxb-2925112561509-2951781580992-ZvIDJZjVCyLxfwb8X5Z4JmJh"
+myToken = "xoxb-2925112561509-2951781580992-nXWvyVc9JInXDBBbeSl97XRk"
 
 
 def post_message(token, channel, text):
@@ -49,34 +49,29 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 post_message(myToken,"#trade", "login success and start")
 
-# print(get_target_price('KRW-XRP', 0.5))
-# print(get_current_price('KRW-XRP'))
-
-# print(get_target_price('KRW-BTC', 0.5))
-# print(get_current_price('KRW-BTC'))
-
-# print(get_target_price('KRW-ETH', 0.5))
-# print(get_current_price('KRW-ETH'))
-
-# print(get_target_price('KRW-BORA', 0.5))
-# print(get_current_price('KRW-BORA'))
-
-# print(get_target_price('KRW-DOGE', 0.5))
-# print(get_current_price('KRW-DOGE'))
 
  # 자동매매 시작
 while True:
      try:
+         #시간 설정
          now = datetime.datetime.now()
          start_time = get_start_time("KRW-XRP")
          end_time = start_time + datetime.timedelta(days=1)
          
          #매수 목표가 설정
-         xrp_target_price = get_target_price("KRW-XRP", 0.3)
+         xrp_target_price = get_target_price("KRW-XRP", 0.4)
          btc_target_price = get_target_price("KRW-BTC", 0.6)
          eth_target_price = get_target_price("KRW-ETH", 0.4)
-         bora_target_price = get_target_price("KRW-BORA", 0.1)
-         doge_target_price = get_target_price("KRW-DOGE", 0.3)
+         bora_target_price = get_target_price("KRW-BORA", 0.3)
+         doge_target_price = get_target_price("KRW-DOGE", 0.5)
+         
+         #잔고 조회    
+         krw = get_balance("KRW")
+         xrp = get_balance("KRW-XRP")
+         btc = get_balance("KRW-BTC")
+         eth = get_balance("KRW-ETH")
+         bora = get_balance("KRW-BORA")
+         doge = get_balance("KRW-DOGE") 
          
          if now == start_time:
              #장 시작시 매수 목표가 알림
@@ -85,76 +80,59 @@ while True:
                         + "\nBTC : "+ str(btc_target_price)
                         + "\nETH : "+ str(eth_target_price) 
                         + "\nBORA : "+ str(bora_target_price)
-                        + "\nDOGE : "+ str(doge_target_price))
-             
-             
+                        + "\nDOGE : "+ str(doge_target_price))   
 
          if start_time < now < end_time - datetime.timedelta(seconds=10):
              # 9900원 이상 보유시 1만원 매수
              #리플
              current_price = get_current_price("KRW-XRP")
              if xrp_target_price < current_price:
-                 krw = get_balance("KRW")
-                 if krw > 9900:
-                     if get_balance("KRW-XRP") > 2:
-                        upbit.buy_market_order("KRW-XRP", 9999)
-                        post_message(myToken,"#trade", "XRP buy")
+                 if krw > 9900 & xrp < 2:
+                     upbit.buy_market_order("KRW-XRP", 9999)
+                     post_message(myToken,"#trade", "XRP buy")
              #비트코인        
              current_price = get_current_price("KRW-BTC")
              if btc_target_price < current_price:
-                 krw = get_balance("KRW")
-                 btc = get_balance("KRW-BTC")
-                 if krw > 9900 & btc > 0.0002:
+                 if krw > 9900 & btc < 0.0002:
                      upbit.buy_market_order("KRW-BTC", 9999)
                      post_message(myToken,"#trade", "BTC buy")
              #이더리움
              current_price = get_current_price("KRW-ETH")
              if eth_target_price < current_price:
-                 krw = get_balance("KRW")
-                 eht = get_balance("KRW-ETH")
-                 if krw > 9900 & eth > 0.002:
+                 if krw > 9900 & eth < 0.002:
                      upbit.buy_market_order("KRW-ETH", 9999)
                      post_message(myToken,"#trade", "ETH buy")
              #보라        
              current_price = get_current_price("KRW-BORA")
              if bora_target_price < current_price:
-                 krw = get_balance("KRW")
-                 bora = get_balance("KRW-BORA")
-                 if krw > 9900 & bora > 2:
+                 if krw > 9900 & bora < 2:
                      upbit.buy_market_order("KRW-BORA", 9999) 
                      post_message(myToken,"#trade", "BORA buy") 
             #도지       
              current_price = get_current_price("KRW-DOGE")
              if doge_target_price < current_price:
-                 krw = get_balance("KRW")
-                 doge = get_balance("KRW-DOGE")
-                 if krw > 9900 & doge > 2:
+                 if krw > 9900 & doge < 2:
                      upbit.buy_market_order("KRW-DOGE", 9999) 
                      post_message(myToken,"#trade", "DOGE buy")       
                      
          else:
              #리플
-             xrp = get_balance("XRP")
              if xrp > 0.00008:
                  upbit.sell_market_order("KRW-XRP", xrp)
                  post_message(myToken,"#trade", "XRP sell")
             #비트코인
-             btc = get_balance("BTC")
              if btc > 0.00008:
                  upbit.sell_market_order("KRW-BTC", btc)
                  post_message(myToken,"#trade", "BTC sell")
              #이더리움
-             eth = get_balance("ETH")
              if eth > 0.00008:
                  upbit.sell_market_order("KRW-ETH", eth)
                  post_message(myToken,"#trade", "ETH sell") 
             #보라
-             bora = get_balance("BORA")
              if bora > 0.00008:
                  upbit.sell_market_order("KRW-BORA", bora)
                  post_message(myToken,"#trade", "BORA sell")
             #도지
-             doge = get_balance("DOGE")
              if bora > 0.00008:
                  upbit.sell_market_order("KRW-DOGE", doge)
                  post_message(myToken,"#trade", "DOGE sell")           
